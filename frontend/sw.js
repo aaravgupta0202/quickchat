24271ebc4877b6465c45648efb8781698a4d9c87
invincible0202 <aarav0202@gmail.com>
2026-07-06 08:28:24 +0530

    v2

---
+++ frontend/sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'quickchat-v1';
+const urlsToCache = [
+  './index.html',
+  './chat.html',
+  './style.css',
+  './icon.svg'
+];
+
+self.addEventListener('install', event => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+      .catch(err => console.warn("Cache add failure (often normal for remote dev):", err))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
